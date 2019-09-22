7c86f48f56f699de6813b5364cbf596f501e90df
Oleg <gogoleg2008@gmail.com>
2019-09-22 22:17:33 +0300

    22.09.2019

--- src/by/bemn/java_intro_online/lesson06_tasks/task1/controller/catalog/Catalog.java
+++ src/by/bemn/java_intro_online/lesson06_tasks/task1/controller/catalog/Catalog.java
@@ -6,6 +6,7 @@ import by.bemn.java_intro_online.lesson06_tasks.task1.entity.user.User;
 import java.util.List;
 
 public interface Catalog {
+
     String show();
     User getClient();
     void setClient(User user);
